e84d6b0505af90406c6890d0af56bc1650542eeb
kedson <kedsonmrs@gmail.com>
2025-05-06 15:55:52 -0300

    atualizando anotaçoes do curso

--- 02-PYTHON-DADOS/PYTHON - TRATAMENTO DADOS.py
+++ 02-PYTHON-DADOS/PYTHON - TRATAMENTO DADOS.py
@@ -32,7 +32,7 @@ pow(4,3) 'parametro 1 - base, parametro 2 - potencia'
 VAR = 1
 
 #IMPRIMINDO VARIAVEL
-print(VAR) or VAR
+print(VAR)
 
 #POSSO DECLARAR MAIS DE UMA VARIAVEL EM UMA MESMA LINHA USANDO VIRGULA
 var1, var2, var3 = 'BOB', 'MARIA', 'ANA'
@@ -162,9 +162,6 @@ max(lista)
 
 #RETORNANDO MENOR VALOR DA LISTA
 min(lista)
-
-#ENUMERATE - TRAZ INDICE E VALOR DA LISTA PARA O LOOP
-enumerate(lista)
 
 #ADICIONANDO VALOR COM APPEND
 #AO ADICIONAR MAIS DE UM ELE INSERE O CONJUNTO DE DADOS EM FORMA DE LISTA SEM DESEMPACOTAR
@@ -551,29 +548,252 @@ var = open('caminho do arquivo','a')
 #FUNCIONA O 'W' e no 'A' (ADICIONA NO FIM)
 var.write("str")
 
-#ABRINDO ARQUIVO .CSV
+#ABRINDO ARQUIVO .CSV e TRANSFORMANDO PARA MANIPULAÇAO EM PYTHON
 #PARA READ É A MESMA FORMA PARA ABRIR
 
-#DIVIDINDO AS LINHAS - SOMENTE SEPARA CADA LINHA EM ITENS
-arq = open('caminho','r')
+#DIVIDINDO AS LINHAS - SOMENTE SEPARA CADA LINHA EM ITENS DE UMA LISTA
+arq = open('caminho','r') #parametro NEWLINE, quebra a linha com o caracter definido (apenas LE OQ JA EXISTE)
 tab = arq.read()
 linhas = linha.split('\n') #aqui separa em uma lista cada ENTER, transformando em valores
+                           #NAO FUNCIONA EM LISTAS
 
 #AGORA EM COLUNAS PARA VIABILIZAR LOCALIZAÇAO VIA INDEX DE CADA VALOR NA TABELA
 full_tab = []
 for linha in linhas:
     split_linha = linhas.split(,)
-    full_tab.append()
+    full_tab.append(split_linha)
 print(full_tab)
 
 #COM ISSO CADA LINHA SEPARADA POR LISTAS , E CADA VALOR (COLUNAS) POR ITENS DENTRO DESSAS LISTAS
 #EXEMPLO
 full_tab[1][2] #linha 1 e coluna 2
 
+#PARA CONTAR QUANTAS LINHAS HA NA TABELA
+count = 0
+for row in full_tab:
+    count += 1
+print(count)
 
+#PARA CONTAR COLUNAS - APENAS USAR FOR PARA A PRIMEIRA LINHA (COLUNA SEPARADA POR VIRGULA)
+row1 = full_tab[0]
+count = 0
+for columns in row1
+    count += 1
+print(count)
 
+#FUNÇAO QUE JA TRAS LINHA A LINHA COMO LISTA, SEPARADO POR \N
+arq.readlines()
 
+#CODIGO ESCRITO POR MIM PRATICA
+#MANIPULANDO O CSV PARA LINHAS E COLUNAS, CONTANDO O NUMERO DE COLUNAS e LINHAS
+arq = open('C:\\Users\\kedso\\Desktop\\CURSOS\\Business_Owners_20250427.csv','r')
+tab = arq.read()            #em paralelo pode usar arq.readlines() pois ja le quebrando as linhas direntamente
+tab_rows = tab.split('\n')  
+tab_full =[]
+countR = 0
+for row in tab_rows:
+    r = row.split(',')
+    tab_full.append(r)
+    countR += 1
+countC = 0
+column = tab_full[0]
+for coluna in column:
+    countC += 1
+print(f"O arquivo contem {countC} colunas e {countR} linhas!")
 
+#PANDAS - PACOTE PARA TABELAS MUITO MELHOR, TRATA CSV
+import pandas as pd
 
+#PARA ABRIR ARQUIVO
+#E IMPRIMIR A TABELA JA FEITA, O PANDAS JA FORMATA TUDO AUTOMATICO UM CSV
+arquivo = ("C:\\Users\\kedso\\Desktop\\CURSOS\\Business_Owners_20250427.csv")
+print(arquivo)
+tabela = pd.read_csv(arquivo)
+print(tabela.head)
 
+#PARA FILTRAR POR UMA COLUNA E CONTAR QUANTOS REGISTROS TEM DE CADA VALOR
+print(tabela["Title"].value_counts())
 
+#ARQUIVO TXT COM PACOTE 'OS'
+#JOIN PERMITE ARMAZENAR O CAMINHO SEPARADO POR VIRGULAS, EM UMA VARIVEL, PORTATIL
+import os as os
+os.path.join('caminho separado por vigula')
+
+#USANDO O WITH
+#ELE DIFERENTE DO OPEN PURO, NAO PRECISA USAR O CLOSE APOS USO, FAZ AUTOMATICO
+with open('C:\\Users\\kedso\\Desktop\\CURSOS\\teste.csv','w') as arquivo
+    conteudo = arquivo.read()
+
+#PACOTE CSV NO PYTHON
+#USADO PARA GRAVAR LINHAS NO ARQUIVO NO FORMATO CSV ATRAVES DA FUNÇAO WRITER e WRITEROW
+#NEWLINE = '' RECOMENDADO PARA NAO OCORRER REDUNDANCIA POIS O PYTHON JA QUEBRA E A BIBLIOTECA CSV TAMBEM
+import csv as csv
+writer = csv.writer(arquivo)
+writer.writerow(('nota1','nota2','nota3')) #cabeçalho
+writer.writerow((6,8,9))
+writer.writerow((7,9,10))
+
+#AGORA PRA LER OS CSV E ARMAZENAR
+#csv.reader(arq) ARMAZENADA EM UMA VARIAVEL ASSOCIADA A UM LOOP LE LINHA POR LINHA DO ARQUIVO
+with open('C:\\Users\\kedso\\Desktop\\CURSOS\\teste.csv','r') as arq:
+    reader = csv.reader(arq) #RETORNA AS LINHAS EM UM ITERAVEL (LISTAS)
+    for x in reader:
+        print(x)
+    dados = list(reader)#TRANSFORMANDO O CONJUNTO DE LISTAS LIDAS EM UMA LISTA DE LISTAS
+    for i in dados[1:]: #AGORA IMPRIMINDO LINHA POR LINHA (LISTA)
+        print(i)
+ 
+#TRANSFORMANDO UM DICIONARIO PYTHON EM JSON (JSON NATURALMENTE É UM DICIONARIO)
+#E EXPORTANDO ELE PARA UM ARQUIVO EM DISCO
+import json as js
+dict_guido = {'nome':'Guido van Rossun',
+              'linguagem':'Python',
+              'similar':['c','Modula-3','lisp']
+              'users':1000000}
+
+json1 = js.dumps(dict_guido) #ESSA FUNÇAO TRANSFORMA UM DICT PYTHON EM JSON
+with open('Arquivo.json','w') as arq:
+    arq.write(json1)
+
+#AGORA LENDO UM ARQUIVO JSON
+with open('Arquivo.json','r') as arq:
+    texto = arq.read()
+    dados = json.loads(texto) #apos isso se consulta como dicionario normal
+    print(dados['nome'])
+
+#PEGANDO ARQUIVO JSON DA WEB
+#UTILIZA BIBLIOTECA EXTERNA
+from urlib.request import urlopen #aqui esta sendo importado apenas uma funçao do pacote
+var = urlopen('endereço do arquivo').read().decode('utf8')
+#apos manipula igual ao normal
+dados = var.loads(var)[0]
+print('titulo: ',dados['title'])
+print('URL: ',dados['url'])
+
+#SIMPLIFICANDO
+#GRAVANDO CONTEUDO DE UM ARQUIVO EM OUTRO
+open('arq','w').write(open('arq2','r').read())
+
+#todos os metodos e atributos do pacote
+dir(numpy)
+
+#o que a funçao faz
+help(sqrt) #por exemplo do numpy
+
+#PACOTE PARA TRATAR ALETORIEDADE
+import random
+
+random.choice([1,2,3,4,5]) #retorna um valor aleatorio
+random.sample(range(100),10) #retorna 10 valores do range 
+
+#ESTATISTICA
+import statistics
+dados[1,2,3,4,5,6,7,8,9,10]
+
+statistics.mean(dados) #retorna media
+statistics.median(dados) #mediana
+
+#MAIS FUNÇOES BUILT IN
+
+#FUNÇAO MAP() - RETORNA ITERADOR
+#APLICA EM QUALQUER ITERAVEL EXECUTANDO DETERMINADA FUNÇAO EM CADA VALOR
+#PODE USAR EM CONJUNTO COM LIST() PARA CRIRAR OUTRA LISTA
+
+lista = list(map('funçao','iteravel'))
+print(lista)
+
+#UTILIZANDO EM CONJUNTO COM LAMBDA - PUXA OS VALORES DA LISTA PARA A OPERAÇAO
+lista1 = (lambda x:x+1,'lista')
+print(lista1)
+
+#MAP TRATA MAIS DE UMA LISTA SE NECESSARIO
+lista1 = (lambda x,y:x+y,'lista','lista2')
+print(lista1)
+ 
+#FUNÇAO REDUCE() - REDUZ UMA LISTA A UM UNICO VALOR, SOMA, MAIOR, MENOR - FUNÇAO COM 2 PARAMETROS
+#RETORNA SEMPRE O X - ACUMULADOR
+res = reduce(lambda x, y:a + y,numeros,'inicial se necessario')
+print(res) #nesse caso retorna a soma de todos os numeros da lista
+
+#AGORA PARA VER MAIOR VALOR
+maior = reduce(lambda x, y:x if (x > y) else y,numeros)
+print(maior) #aqui retorna o maior valor por comparaçao
+
+#FUNÇAO FILTER() - RETORNA ITERAVEL APOS FILTRO - aplica a cada item do iteravel
+#COM O ITERAVEL SE FAZ O LOOP OU CRIA UMA LISTA
+filter('funçao retornando booleano, filtro','iteravel')
+filter(lambda x:x % 2 == 0,numeros) #retornara apenas os que satisfazerem ao filtro (par)
+
+#ZIP(), RETORNA A JUNÇAO DE 2 LISTAS - ITEM POR ITEM TRAZ PARES ENTRE ELAS EM FORMA DE TUPLAS
+#RETORNA ITERADOR, UMA LISTA DE TUPLAS
+x = [1,2,3,4]
+y = [5,6,7,8]
+print(list(zip(x,y)))
+
+#EM DICIONARIO ELE RETORNA A CHAVE, PARA VOLTAR VALOR DEVE ESPECIFICAR O .values()
+zip(x,y.values())
+
+#ENUMERATE - TRAZ INDICE DOS VALORES E OS VALORES EM PARES NAS TUPLAS DE FORMA DE ITERAVEL (PARECIDO COM ZIP)
+enumerate(lista)
+
+#TRATAMENTO DE ERRO
+
+try:  #TENTE FAZER - PARECE BASTANTE O IF, MAS PODE USAR COM ERRO ESPECIFICO
+    'linhas de programa normal'
+except IOError: #SE DER ERRO EMITA ESSA MENSAGEM
+    print('mensagem')
+else: #SE DER CERTO EMITA ESSA
+    print('executado com sucesso')
+finally: #ESSA SERA EMITIDA DE QUALQUER FORMA
+    print('sempre printa')
+    
+#EXPRESSOES REGULARES - PADROES USADOS PARA COMBINAR OU ENCONTRAR OCORRENCIAS DE SEQUENCIAS DE CARACTERES EM UMA STRING
+#TABELA COM OS CORRESPONDENTES
+🔤 Caracteres e Classes de Caracteres
+Símbolo	    Significado	                                    Exemplo
+.	        Qualquer caractere (exceto \n)	                a.b → casa: acb, arb
+\w	        Letra, número ou _ (equivale a [A-Za-z0-9_])	\w+ → joao123, a_b
+\W	        Tudo que não é \w	                            Espaço, @, #, etc.
+\d	        Qualquer dígito (0–9)	                        \d+ → 12345
+\D	        Tudo que não é dígito	                        abc, @, etc.
+\s	        Espaço em branco (espaço, tab, quebra de linha)	
+\S	        Qualquer coisa que não seja espaço
+
+Quantificadores
+Símbolo	    Significado	            Exemplo
++	        1 ou mais vezes	        a+ → a, aa, aaa
+*	        0 ou mais vezes	        a* → ``, a, aaaa
+?	        0 ou 1 vez	            a? → ``, a
+{n}	        Exatamente n vezes	    \d{2} → 22, 08
+{n,}	    Pelo menos n vezes	    \d{3,} → 123, 5678
+{n,m}	    Entre n e m vezes	    \d{2,4} → 12, 345, 6789
+
+🔘 Âncoras e Fronteiras
+Símbolo	        Significado	                    Exemplo
+^	            Início da string	            ^Olá → só se começar com Olá
+$	            Fim da string	                fim$ → só se terminar com fim
+\b	            Fronteira de palavra	        \babc\b → só abc isolado
+\B	            Não é fronteira de palavra	    \Babc → parte de outra palavra
+
+import re
+
+#ACHANDO NUMERO DE VEZES QUE APARECE UM CARACTER NUMA STRING - RETORNA INT COM LEN() 
+txt = "Meu email é kkkkkkkk@gmail.com e voce pode me contatar em outro_kkkk@gmail.com"
+rst = len(re.findall('@',txt))
+print (rst)
+
+#PALAVRAS QUE VEM APOS UMA ESPECIFICAR - lista com as string
+rst1 = re.findall(r'voce (\w+)', txt) #CONSIDERA ESPAÇO 
+print(rst1[0])
+
+#BASICAMENTE SE MONTA A PALAVRA DA BUSCA
+#POR EXEMPLO QUERO EMAIL
+re.findall(r'\w+@\w+\.[a-zA-Z]+',txt)
+#MAIS EXEMPLOS
+len(re.findall(r'a',txt) #quantos A tem no texto
+len(re.findall(r'\btempo\b',txt) #quantos TEMPO tem na palavra
+re.findall(r'\w+!',txt) #palavras seguidas por '!'
+re.findall(r'esse\s(\w+)\samargo',txt) #palavra entre 'esse' e 'amargo 
+                                       #uso de intervalo dever dar match EXATO
+                                       #O PARENTESES SERVE PRA DEFINIR O QUE VOCER QUER PEGAR, FORA FICA O "INTERVALO"
+re.findall(r'\w+[á-úÁ-Ú]\b',txt)#\b MINUSCULO POIS ACENTUADO = \W e letra normal \w
